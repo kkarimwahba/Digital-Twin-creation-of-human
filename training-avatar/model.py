ba432211492427a4a0d5fa4d0c194a00c6e58cbb
khaledsho3er <khaledamrahmed0@gmail.com>
2024-03-02 23:41:23 +0200

    training artibuttes

--- training-avatar/model.py
+++ training-avatar/model.py
@@ -10,3 +10,10 @@ import os
 from tensorflow.keras.preprocessing import image  # For image loading
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
+
+image_height, image_width, num_channels = 128, 128, 3  # Image dimensions and channels
+latent_dim = 100  # Dimensionality of the latent space
+num_vertices = 1000  # Example number of vertices for the mesh
+epochs = 10  # Training epochs
+batch_size = 32  # Training batch size
+learning_rate = 0.0005  # Learning rate for Adam optimizer (adjust based on experiment results)
